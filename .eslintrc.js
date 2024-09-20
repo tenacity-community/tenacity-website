133878b828af258fb4b3bb3c23eb235602f61137
SagarNRao <sagarnrao154@gmail.com>
2024-09-20 23:10:48 +0530

    one more time - 2

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+const isVercel = process.env.DISABLE_ESLINT_RULES === 'true';
+
+module.exports = {
+  env: {
+    node: true, // Add this line to specify Node.js environment
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': isVercel ? 'off' : 'error',
+    'no-unused-vars': isVercel ? 'off' : 'error',
+    '@typescript-eslint/no-unused-vars': isVercel ? 'off' : 'error',
+  },
+};
